44ff1fbf410d053cead9acbb18d5e62dfbd397f7
arpanghosh-dev <arpan.ghosh@weavers-web.com>
2026-04-20 19:03:54 +0530

    fix-comment

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from 'react'
 import { PageLoader } from '@/components/common/Loader/Loader'
-import AppFooter from '@/components/common/AppFooter/AppFooter'
+// import AppFooter from '@/components/common/AppFooter/AppFooter'
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <Suspense fallback={<PageLoader />}>
       <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
         <main style={{ flex: 1 }}>{children}</main>
-        <AppFooter />
+        {/* <AppFooter /> */}
       </div>
     </Suspense>
   )
--- src/proxy.ts
+++ src/proxy.ts
@@ -41,8 +41,14 @@ export function proxy(req: NextRequest) {
     return NextResponse.redirect(new URL('/landing', req.url))
   }
 
+  // I am done inspecting this request. Continue normal processing. , do not block , redirect , or rewrte , just go for next step.
   return NextResponse.next()
 }
+
+// This controls where proxy runs.
+// without this matcher proxy will run on every route including static assets. eg: images, css, 
+// js buindels, fonts 
+// this rejex means run proxy on everything except _next/static, _next/image, favicon.ico and everything in public folder
 
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico|public/).*)'],
